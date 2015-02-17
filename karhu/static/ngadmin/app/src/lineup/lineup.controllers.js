6e3de851029c82089a9495c9d7fad96874867cc1
Ivan Arkhipov <mannaz13@gmail.com>
2015-02-17 19:12:41 +0400

    Separate notes list interface

--- karhu/static/ngadmin/app/src/lineup/lineup.controllers.js
+++ karhu/static/ngadmin/app/src/lineup/lineup.controllers.js
@@ -57,86 +57,17 @@
     mdl.controller('LineupPersonCtrl', ['$scope', '$q', '$state', '$modal', 'APP_ROOT_FOLDER', 'Lineup', 'SingleFileUploader', 'resolvedData',
         function ($scope, $q, $state, $modal, ROOT, Lineup, SingleFileUploader, resolvedData) {
 
-            function getBlankNoteFor(topic) {
-                var note = Lineup.Note.getOne(null);
-                //note.person = person_id;
-                note.person = $scope.person.id;
-                note.topic = topic.id;
-                note.text = '';
-                note.local = {};
-                return note;
-            }
-            
-            function getNoteFor(notes, topic) {
-                var result;
-                ng.forEach(notes, function (note) {
-                    if (note.topic === topic.id) {
-                        result = note;
-                    }
-                });
-                if (!result) { result = getBlankNoteFor(topic); }
-                return result;
-            }
-            
-            // Note is always related to some Person and cannot be saved without person.id
-            // Note with any text will be created or updated depending on if it already has .id
-            // Note without text will be deleted
-            function saveNote(topic) {
-                var isEmpty = topic.note.text.trim() === '',
-                    hasToBeDeleted = isEmpty && topic.note.id,
-                    hasToBeSaved = !isEmpty,
-                    result = null;
-                
-                if ($scope.person.id) {
-                    if (hasToBeDeleted) {
-                        result = Lineup.Note.remove(topic.note);
-                        result.then(function () {
-                            topic.note = getBlankNoteFor(topic);
-                        });
-                    }
-                    if (hasToBeSaved) {
-                        if (!topic.note.person) {
-                            console.log('adding p[erson id');
-                            topic.note.person = $scope.person.id;
-                        }
-                        result = Lineup.Note.save(topic.note);
-                        result.then(function (response) {
-                            topic.note = response;
-                        });
-                    }
-                }
-                return $q.when(result);
-            }
-            
-            function batchSaveNotes() {
-                var requests = [];
-                ng.forEach($scope.topics, function (topic) {
-                    //if (topic.note.text.trim() !== '' || (topic.note.id && topic.note.text.trim() === '')) {
-                    requests.push(saveNote(topic));
-                    //}
-                });
-                return $q.all(requests);
-            }
-            
-            
             $scope.is = {
                 clearing_cover: false,
                 saving: false,
                 deleting: false
             };
-            //$scope.person = {};
             
             $scope.uploader = SingleFileUploader.create({
                 uploadTo: function () {
                     return Lineup.Person.getUploadUrl($scope.person.id);
-                },
-                onSuccess: function (item, response) {
-                    $scope.is.saving = false;
-                    $scope.person.photo = SingleFileUploader.randomizeUrl(response);
-                },
-                onError: function (item, response) {
-                    $scope.is.saving = false;
                 }
+
             });
             
             $scope.cropImage = function (selection) {
@@ -149,42 +80,19 @@
                     });
             };
             
-            
-            $scope.delete_note = function (topic) {
-                if (topic.note.id) {
-                    topic.note.local.isPending = true;
-                    Lineup.Note
-                        .remove(topic.note)
-                        .then(function () {
-                            topic.note.local.isPending = false;
-                            topic.note = getBlankNoteFor(topic);
-                        });
-                }
-            };
-            
-//            $scope.create_topic = function () {
-//                if ($scope.newtopic !== '') {
-//                    var data = {title: $scope.newtopic};
-//                    Lineup.Topic.save(data).then(function (response) {
-//                        response.note = getBlankNoteFor(response);
-//                        $scope.topics.push(response);
-//                        $scope.newtopic = '';
-//                    });
-//                }
-//            };
-
-//            $scope.delete_topic = function (topic) {
-//                Lineup.Topic.removeFromList($scope.topics, topic);
-//            };
 
             $scope.savePerson = function () {
                 $scope.is.saving = true;
                 Lineup.Person
                     .save($scope.person)
                     .then(function (response) {
-//                        console.log('first then', response);
                         $scope.person = response;
-                        return batchSaveNotes();
+//                        return batchSaveNotes();
+//                        var requests = [];
+//                        ng.forEach($scope.topics, function (topic) {
+//                            requests.push(saveNote(topic));
+//                        });
+//                        return $q.all(requests);
                     })
                     .then(function (response) {
                         $scope.uploader
@@ -199,47 +107,23 @@
             };
             
             $scope.removePerson = function () {
+                $scope.is.deleting = true;
                 Lineup.Person
                     .remove($scope.person)
                     .andGo('lineup.list');
             };
             
 
-            $scope.openTopicsEditForm = function () {
-                var modal = $modal.open({
-                    controller: 'ModalTopicsEditCtrl',
-                    templateUrl: ROOT + 'lineup/templates/modal-topics-edit.html'
-                });
-                modal.result
-                    .then(function (result) {
-                        $scope.topics.length = 0;
-                        $scope.topics = result;
-                    });
-            
-            };
-        
 
             /*     ------------     */
             
-//            $scope.newtopic = '';
             $scope.person = resolvedData[0];
             $scope.notes = resolvedData[2];
             
-            var topics = resolvedData[1];
-                
-            // Each topic gets either matched note or blank note
-            if ($scope.person.id) {
-                ng.forEach(topics, function (topic) {
-                    topic.note = getNoteFor($scope.notes, topic);
-                });
-            } else {
-                ng.forEach(topics, function (topic) {
-                    topic.note = getBlankNoteFor(topic);
-                });
-            }
-            $scope.topics = topics;
-            
+            $scope.topics = resolvedData[1];
             $scope.config = Lineup.Person.config;
+            
+            
             
             
         }]);
--- karhu/static/ngadmin/app/src/lineup/lineup.directives.js
+++ karhu/static/ngadmin/app/src/lineup/lineup.directives.js
@@ -3,27 +3,148 @@
     'use strict';
     var mdl = ng.module('LineupModule');
     
-    mdl.directive('modalTopicsEdit',
+    mdl.controller('NotesListCtrl', ['$scope',  '$q', '$modal', 'APP_ROOT_FOLDER', 'Lineup', function ($scope, $q, $modal, ROOT, Lineup) {
+                
+        
+        function getBlankNoteFor(topic) {
+            var note = Lineup.Note.getOne(null);
+            //note.person = person_id;
+            note.person = $scope.person.id;
+            note.topic = topic.id;
+            note.text = '';
+            note.local = {};
+            return note;
+        }
+        
+        function getNoteFor(notes, topic) {
+            var result;
+            ng.forEach(notes, function (note) {
+                if (note.topic === topic.id) {
+                    result = note;
+                }
+            });
+            if (!result) { result = getBlankNoteFor(topic); }
+            return result;
+        }
+        // Note is always related to some Person and cannot be saved without person.id
+        // Note with any text will be created or updated depending on if it already has .id
+        // Note without text will be deleted
+        $scope.saveNote = function (topic) {
+            var isEmpty = topic.note.text.trim() === '',
+                hasToBeDeleted = isEmpty && topic.note.id,
+                hasToBeSaved = !isEmpty,
+                result = null;
+
+            if (hasToBeDeleted) {
+                result = Lineup.Note
+                    .remove(topic.note)
+                    .then(function () {
+                        topic.note = getBlankNoteFor(topic);
+                    });
+            } else if (hasToBeSaved) {
+//                if (!topic.note.person) {
+//                    console.log('adding p[erson id');
+//                    topic.note.person = $scope.person.id;
+//                }
+                result = Lineup.Note
+                    .save(topic.note)
+                    .then(function (response) {
+                        topic.note = response;
+                        topic.note.local.isEdited = false;
+//                        $scope.toggleEditMode(topic.note);
+                    });
+            }
+            
+        };
+
+        $scope.toggleEditMode = function (note) {
+            
+            if (note.local.isEdited) {
+                note.local.isEdited = false;
+            } else {
+                note.local.isEdited = true;
+            }
+            
+        };
+        
+        $scope.delete_note = function (topic) {
+            if (topic.note.id) {
+                topic.note.local.isDeleting = true;
+                Lineup.Note
+                    .remove(topic.note)
+                    .then(function () {
+                        topic.note.local.isDeleting = false;
+                        topic.note = getBlankNoteFor(topic);
+                    });
+            }
+        };
+        
+
+        $scope.openTopicsEditForm = function () {
+            var modal = $modal.open({
+                controller: 'ModalTopicsEditCtrl',
+                templateUrl: ROOT + 'lineup/templates/modal-topics-edit.html'
+            });
+            modal.result
+                .then(function (result) {
+                    $scope.topics.length = 0;
+                    $scope.topics = result;
+                });
+
+        };
+        // Each topic gets either matched note or blank note
+//        var topics = resolvedData[1];
+        
+        console.log($scope.notes);
+        ng.forEach($scope.topics, function (topic) {
+            topic.note = getNoteFor($scope.notes, topic);
+        });
+//        $scope.topics = topics;
+        
+    }]);
+
+    
+    mdl.directive('lineupNotesList',
         ['$state', '$modal',  'APP_ROOT_FOLDER', 'Lineup',
             function ($state, $modal, ROOT, Lineup) {
 
                 return {
-                    restrict: 'A',
-//                    scope: {},
+                    restrict: 'E',
+                    templateUrl: ROOT + '/lineup/templates/notes-list.html',
+                    scope: {
+                        person: '=',
+                        topics: '=',
+                        notes: '='
+                    },
+                    controller: 'NotesListCtrl',
                     link: function ($scope, elt, args) {
-//                        $scope.settings = $scope.modalItemAdd;
-                        elt.click(function () {
-                            var modal = $modal.open({
-                                templateUrl: ROOT + 'lineup/templates/modal-topics-edit.html'
-//                                controller: 'modalAddCtrl',
-//                                scope: $scope,
-//                                resolve: {
-//                                    Service: function () {return $scope.settings.service; }
-//                                }
-                            });
-                        });
+                        
+                        
                     }
                 };
             }]);
+    
+//    mdl.directive('modalTopicsEdit',
+//        ['$state', '$modal',  'APP_ROOT_FOLDER', 'Lineup',
+//            function ($state, $modal, ROOT, Lineup) {
+//
+//                return {
+//                    restrict: 'A',
+////                    scope: {},
+//                    link: function ($scope, elt, args) {
+////                        $scope.settings = $scope.modalItemAdd;
+//                        elt.click(function () {
+//                            var modal = $modal.open({
+//                                templateUrl: ROOT + 'lineup/templates/modal-topics-edit.html'
+////                                controller: 'modalAddCtrl',
+////                                scope: $scope,
+////                                resolve: {
+////                                    Service: function () {return $scope.settings.service; }
+////                                }
+//                            });
+//                        });
+//                    }
+//                };
+//            }]);
 
 }(angular));
