80d69702fe92fd9e7f275467b7eca24b9ea3e7e9
Ivan Arkhipov <mannaz13@gmail.com>
2015-03-10 16:28:48 +0400

    Separate customMatchers for tests

--- karhu/static/ngadmin/test/karma.conf.js
+++ karhu/static/ngadmin/test/karma.conf.js
@@ -64,7 +64,8 @@ module.exports = function(config) {
         	'app/src/**/*.js',
         	'app/src/*.js',
             'app/src/app.js',
-              
+            
+            'test/spec/custom-matchers.js',
         	'test/spec/**/*.js',
             'test/mock/**/*.js'
     ],
@@ -104,7 +105,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
